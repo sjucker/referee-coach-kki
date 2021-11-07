38d3e3abef0ac6b09bdec7e7712c2545c1bb9d30
Stefan Jucker <jucker@mptechnology.ch>
2021-11-07 18:02:23 +0100

    Fix view-link

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -4,23 +4,23 @@ import {MainComponent} from "./main/main.component";
 import {ViewExpertiseComponent} from "./view-expertise/view-expertise.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: MainComponent
-  },
-  {
-    path: 'view/:id',
-    component: ViewExpertiseComponent
-  },
-  {
-    path: 'edit/:id',
-    component: MainComponent
-  }
+    {
+        path: '',
+        component: MainComponent
+    },
+    {
+        path: 'view/:id',
+        component: ViewExpertiseComponent
+    },
+    {
+        path: 'edit/:id',
+        component: MainComponent
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
