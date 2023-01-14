3dc1d7cd9834d3f2c280aab9d12dff848eae8d4d
Stefan Jucker <jucker@mptechnology.ch>
2023-01-14 15:11:50 +0100

    Handle unknown user better (should not result in 500 error)

--- src/main/java/ch/stefanjucker/refereecoach/security/JwtRequestFilter.java
+++ src/main/java/ch/stefanjucker/refereecoach/security/JwtRequestFilter.java
@@ -6,7 +6,7 @@ import io.jsonwebtoken.Claims;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -38,11 +38,16 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             Optional<Claims> jwt = jwtService.validateJwt(jwtToken);
             if (jwt.isPresent()) {
                 var email = jwt.get().getSubject();
-                UserDetails userDetails = userAuthService.loadUserByUsername(email);
-
-                var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                try {
+                    var userDetails = userAuthService.loadUserByUsername(email);
+                    var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } catch (UsernameNotFoundException e) {
+                    // just continue unauthenticated if username unknown (e.g, email needed changing) => user gets logged out
+                    logger.warn("could not authenticate user, since username was unknown", e);
+                    filterChain.doFilter(request, response);
+                }
             }
         }
         filterChain.doFilter(request, response);
